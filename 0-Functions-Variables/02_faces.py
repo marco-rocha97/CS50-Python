8e89f0fdd9eff683a7c694ef38e1724e5fd3f52f
Marco Aurélio Pereira Rocha <145925056+marco-rocha97@users.noreply.github.com>
2023-12-04 20:55:13 -0300

    Add files via upload

---
+++ 0-Functions-Variables/02_faces.py
@@ -0,0 +1,15 @@
+# In a file called faces.py, implement a function called convert that accepts a str as input and returns that same input with any :) converted to 🙂 (otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face). 
+# All other text should be returned unchanged.
+
+# Then, in that same file, implement a function called main that prompts the user for input, calls convert on that input, and prints the result. 
+# You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input. Be sure to call main at the bottom of your file.
+def convert(sentence):
+    sentence = sentence.replace(":)", "🙂")
+    sentence = sentence.replace(":(", "🙁")
+    return sentence
+
+def main():
+    x = str(input("Type your phrase: "))
+    print(convert(x))
+
+main()
---
+++ 0-Functions-Variables/03_einstein.py
@@ -0,0 +1,6 @@
+# In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer. 
+# Assume that the user will input an integer.
+m = int(input("Type the mass: "))
+c = 300000000
+E = m*(c**2)
+print(E)
